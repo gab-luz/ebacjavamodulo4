de944873088c3757bae7c9931bda228a5fc381a5
Andrius Gabriel da Luz <gabrielluz@riseup.net>
2023-10-22 18:53:45 -0300

    Enviado itens para o módulo 5

---
+++ src/com/gabriel/luzm4/Modulo4.java
@@ -0,0 +1,9 @@
+package com.gabriel.luzm4;
+
+public class Modulo4 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.print("Módulo 4");
+
+    }
+}
